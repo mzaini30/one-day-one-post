11c26cf1c0bb960884e88eff8b14d7dec7f6b510
mzaini30 <muhzaini30@gmail.com>
2019-01-28 07:29:01 +0800

    oke

--- merancang/_site/app/app.js
+++ merancang/_site/app/app.js
@@ -1,3 +1,15 @@
 $('.navbar-nav a').click(function(){
 	$('.navbar-toggle').click()
 })
+
+$('.acak').click(function(){
+	$('.list-group-item').shuffle()
+})
+
+$('.urut').click(function(){
+	els = $('.list-group-item').get()
+	els.sort(function(el1, el2){
+	    return $(el1).text().trim().localeCompare($(el2).text().trim())
+	})
+	$('.list-group').append(els)
+})
